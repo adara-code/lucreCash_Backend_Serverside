45ae670458d6183dfe1833fbb88377ec7bbe9943
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-01-17 18:22:34 +0300

    added a response message in the else statement for the user whose details successfully pass through data validation

--- controllers/userCredentials.js
+++ controllers/userCredentials.js
@@ -9,8 +9,10 @@ const signup = async(req,res) => {
     const signupValidation = await signupSchema.validate(req.body)
     if(signupValidation.error) {
         console.log(signupValidation.error.details[0].message)
-        res.status(200).json([{message:signupValidation.error.details[0].message}])
+        res.status(200).json([{message: signupValidation.error.details[0].message}])
+        
     } else {
+        res.status(200).json([{message:"Registration successful"}])
         console.log("Credentials successful")
     }
     
@@ -22,15 +24,11 @@ const login = async(req,res) => {
     } else {
         console.log("Login validation successful")
     }
-    // console.log("login route")
-    // res.status(200).json([{message: "login route is working"}])
 }
 
 module.exports = {signup, login}
 
 
 
-//creates the user table in the database
-// sequelize.sync().then(result => {
-//     console.log(result)
-// })
+/*creates the user table in the database
+sequelize.sync().then(result => console.log(result)) */
