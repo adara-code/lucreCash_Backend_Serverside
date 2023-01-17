37893a378740b56b95f36422d148fa9ecf978b72
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-01-17 20:32:25 +0300

    added the bcrypt package, hashed the incoming password and saved the users details as an object

--- controllers/userCredentials.js
+++ controllers/userCredentials.js
@@ -1,32 +1,50 @@
 const Joi = require('joi')
+const bcrypt = require('bcrypt')
 const sequelize = require('../config/connection.js')
 const { signupSchema, loginSchema } = require('../validation/dataValidation.js')
-
 const user = require('../models/Users.js')
 
 
-const signup = async(req,res) => {
+const salt = bcrypt.genSaltSync(10)
+
+const signup = async (req, res) => {
     const signupValidation = await signupSchema.validate(req.body)
-    if(signupValidation.error) {
+    if (signupValidation.error) {
         console.log(signupValidation.error.details[0].message)
-        res.status(200).json([{message: signupValidation.error.details[0].message}])
-        
+        res.status(200).json([{ message: signupValidation.error.details[0].message }])
+
     } else {
-        // res.status(200).json(["Registration successful"])
+        const user = {
+            fullnames : signupValidation.value.fullnames,
+            inputUsername : signupValidation.value.username,
+            inputEmail : signupValidation.value.email,
+            inputPassword : bcrypt.hashSync(signupValidation.value.password, salt)
+        }
+
+        console.log(user)
+
+        // user.create({
+        //     fullnames : inputFullnames,
+        //     username : inputUsername,
+        //     email: inputEmail,
+        //     password: inputPassword
+        // })
+        // // res.status(200).json(["Registration successful"])
         // console.log("Credentials successful")
     }
-    
+
 }
-const login = async(req,res) => {
+
+const login = async (req, res) => {
     const loginValidation = await loginSchema.validate(req.body)
-    if(loginValidation.error){
-        res.status(200).json([{message:loginValidation.error.details[0].message}])
+    if (loginValidation.error) {
+        res.status(200).json([{ message: loginValidation.error.details[0].message }])
     } else {
         console.log("Login validation successful")
     }
 }
 
-module.exports = {signup, login}
+module.exports = { signup, login }
 
 
 
