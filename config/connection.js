dfaa9e651ac681ac0daf968e7196ea74cb313109
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-02-14 15:10:36 +0300

    changed connection settings

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,11 @@ const dotenv = require('dotenv')
 
 dotenv.config({path: '../.env'})
 
-const sequelize = new Sequelize("lucre","root","gue55me", {
+const sequelize = new Sequelize(process.env.DATABASE,process.env.DATABASE_USER,process.env.PASSWORD, {
     dialect: "mysql",
     host: "localhost"
 })
 
-// const sequelize = new Sequelize(process.env.DATABASE,process.env.DATABASE_USER,process.env.PASSWORD, {
 
 
 // console.log(process.env.DATABASE)
