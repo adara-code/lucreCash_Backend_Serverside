4e06d3f2886e41641c7974a0b3611f598958b565
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-09 13:48:36 +0300

    changed the signup function

--- controllers/userCredentials.js
+++ controllers/userCredentials.js
@@ -29,118 +29,114 @@ that the email or username is taken, if not, a new User object is inserted into 
 // }
 
 const signup = async (req, res) => {
-    const userEmail = req.body.emailSignup
-    const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
+    // const userEmail = req.body.emailSignup
+    // const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
 
-    UserSignUp.findAll({
-        where: {
-            email : userEmail
-        }
-    }).then(rs => {
-        if(rs.length >= 1) {
-            res.status(200).json([{message: "Email taken. Try again"}])
-        } else {
-            UserSignUp.create({
-                email : userEmail,
-                password: userPassword
-            }).then(rs => {
-                const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
-                res.status(200).json([{message: "Signup successful"}])
-                console.log(userToken)
-            })
-        }
-    }).catch(err => {
-        console.log(err)
-    })
+    // UserSignUp.findAll({
+    //     where: {
+    //         email : userEmail
+    //     }
+    // }).then(rs => {
+    //     if(rs.length >= 1) {
+    //         res.status(200).json([{message: "Email taken. Try again"}])
+    //     } else {
+    //         UserSignUp.create({
+    //             email : userEmail,
+    //             password: userPassword
+    //         }).then(rs => {
+    //             const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
+    //             res.status(200).json([{message: "Signup successful"}])
+    //             console.log(userToken)
+    //         })
+    //     }
+    // }).catch(err => {
+    //     console.log(err)
+    // })
     // res.status(200).json([{message: "Signup successful"}])
     // res.status(200).json([{message: "Touchdown"}])
 
     
-    // const signupValidation = await signupSchema.validate(req.body)
+    const signupValidation = await signupSchema.validate(req.body)
 
-    // if (signupValidation.error) {
-    //     console.log(signupValidation.error.details[0].message)
-    //     res.status(200).json([{ message: signupValidation.error.details[0].message }])
+    if (signupValidation.error) {
+        console.log(signupValidation.error.details[0].message)
+        res.status(200).json([{ message: signupValidation.error.details[0].message }])
 
-    // } else {
-    //     User.findAll({
-    //         where: {
-    //             email: signupValidation.value.email
-    //         }
-    //     }).then(rs => {
-    //         if (rs.length >= 1) {
-    //             res.status(200).json([{ message: "Email already exists" }])
-    //         } else {
-    //             User.findAll({
-    //                 where: {
-    //                     username: signupValidation.value.username
-    //                 }
-    //             }).then(rs => {
-    //                 if (rs.length >= 1) {
-    //                     res.status(200).json([{ message: "Username taken" }])
-    //                 } else {
-    //                     User.create({
-    //                         fullnames: signupValidation.value.fullnames,
-    //                         username: signupValidation.value.username,
-    //                         email: signupValidation.value.email,
-    //                         password: bcrypt.hashSync(signupValidation.value.password, salt)
-    //                     }).then(rs => {
-    //                         console.log(rs)
-    //                         res.status(200).json([{ message: "Registration successfull" }])
-    //                     }).catch(err => {
-    //                         console.log(err)
-    //                         res.status(200).json([{ message: "Error: Registation Failed" }])
-    //                     })
-    //                 }
-    //             }).catch(err => {
-    //                 console.log(err)
-    //             })
-    //         }
-    //     }).catch(err => {
-    //         console.log(err)
-    //     })
-    // }
-    res.status(200).json([{message: "touchdown"}])
-}
-
-const login = async (req, res) => {
-    // const loginValidation = await loginSchema.validate(req.body)
-    // if (loginValidation.error) {
-    //     res.status(200).json([{ message: loginValidation.error.details[0].message }])
-    // } else {
-    //     User.findOne({
-    //         where: {
-    //             username: loginValidation.value.username,
-    //         }
-    //     }).then(rs => {
-    //         if (rs == null) {
-    //             // console.log(rs)
-    //             res.status(200).json([{ message: "Username or Password is invalid" }])
-    //         } else {
-    //             const passwordCheck = bcrypt.compareSync(loginValidation.value.password, rs.dataValues.password)
-    //             if (!passwordCheck) {
-    //                 res.status(200).json([{ message: "Username or Password is invalid" }])
-    //             } else {
-    //                 const tokenGenerator = jwt.sign(rs.dataValues,process.env.JWT_KEY)
-    //                 res.status(200).json([{ message: tokenGenerator }])
-    //             }
-    //             console.log(passwordCheck)
-    //         }
-    //         // console.log(res)
-    //         // res.status(200).json([{message: rs}])
-    //     }).catch(err => {
-    //         console.log(err)
-    //     })
-    //     // console.log("Login validation successful")
-    // }
+    } else {
+        User.findAll({
+            where: {
+                email: signupValidation.value.email
+            }
+        }).then(rs => {
+            if (rs.length >= 1) {
+                res.status(200).json([{ message: "Email already exists" }])
+            } else {
+                User.findAll({
+                    where: {
+                        username: signupValidation.value.username
+                    }
+                }).then(rs => {
+                    if (rs.length >= 1) {
+                        res.status(200).json([{ message: "Username taken" }])
+                    } else {
+                        User.create({
+                            fullnames: signupValidation.value.fullnames,
+                            username: signupValidation.value.username,
+                            email: signupValidation.value.email,
+                            password: bcrypt.hashSync(signupValidation.value.password, salt)
+                        }).then(rs => {
+                            console.log(rs)
+                            res.status(200).json([{ message: "Registration successfull" }])
+                        }).catch(err => {
+                            console.log(err)
+                            res.status(200).json([{ message: "Error: Registation Failed" }])
+                        })
+                    }
+                }).catch(err => {
+                    console.log(err)
+                })
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
     // res.status(200).json([{message: "touchdown"}])
 }
 
-sequelize.sync({force: true}).then(rs => {
-    console.log(rs)
-}).catch(err => {
-    console.log(err)
-})
+const login = async (req, res) => {
+    const loginValidation = await loginSchema.validate(req.body)
+    if (loginValidation.error) {
+        res.status(200).json([{ message: loginValidation.error.details[0].message }])
+    } else {
+        User.findOne({
+            where: {
+                username: loginValidation.value.username,
+            }
+        }).then(rs => {
+            if (rs == null) {
+                // console.log(rs)
+                res.status(200).json([{ message: "Username or Password is invalid" }])
+            } else {
+                const passwordCheck = bcrypt.compareSync(loginValidation.value.password, rs.dataValues.password)
+                if (!passwordCheck) {
+                    res.status(200).json([{ message: "Username or Password is invalid" }])
+                } else {
+                    const tokenGenerator = jwt.sign(rs.dataValues,process.env.JWT_KEY)
+                    res.status(200).json([{ message: tokenGenerator }])
+                }
+                console.log(passwordCheck)
+            }
+            // console.log(res)
+            // res.status(200).json([{message: rs}])
+        }).catch(err => {
+            console.log(err)
+        })
+        // console.log("Login validation successful")
+    }
+    // res.status(200).json([{message: "touchdown"}])
+}
+
+
 
 module.exports = { signup, login }
 
@@ -148,5 +144,11 @@ module.exports = { signup, login }
 
 /*creates the user table in the database | Add it to the signup function
 sequelize.sync({ force: true }).then(result => {User.create(user object) console.log(result)}) */
+
+/*sequelize.sync({force: true}).then(rs => {
+    console.log(rs)
+}).catch(err => {
+    console.log(err)
+})*/
 
 
